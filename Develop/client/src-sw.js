239de1a4f1bc45b463ba64b5b6f4fe1feb6a3520
Kathy Hyde <musekat3@gmail.com>
2023-10-29 14:07:13 -0700

    src-sw

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -19,12 +19,32 @@ const pageCache = new CacheFirst({
   ],
 });
 
+
 warmStrategyCache({
   urls: ['/index.html', '/'],
   strategy: pageCache,
 });
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+
+workbox.routing.registerRoute(
+  ({request}) => request.destination === 'image',
+  imageCache
+);
+
+// Implement asset caching
+const imageCache = new workbox.strategies.CacheFirst({
+  cacheName: 'image-cache',
+  plugins: [
+    new workbox.cacheableResponse.CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new workbox.expiration.ExpirationPlugin({
+      maxEntries: 60,
+      maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+    }),
+  ],
+});
 
 // TODO: Implement asset caching
 registerRoute(({ request }) => request.destination === 'image', assetCache);
