d7faab02be48d65f6a54d089262c31ab4d956c70
Kathy Hyde <musekat3@gmail.com>
2023-10-30 22:54:14 -0700

    htmlRoutes

--- Develop/server/routes/htmlRoutes.js
+++ Develop/server/routes/htmlRoutes.js
@@ -1,6 +1,19 @@
+// const path = require('path');
+
+// module.exports = (app) =>
+//   app.get('/', (req, res) =>
+//     res.sendFile(path.join(__dirname, '../client/index.html'))
+//   );
+
+
 const path = require('path');
 
-module.exports = (app) =>
-  app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '../client/index.html'))
-  );
+module.exports = (app) => {
+ const filePath = path.join(__dirname, '../client/index.html');
+ 
+ console.log(`File path: ${filePath}`);
+
+ app.get('/', (req, res) =>
+   res.sendFile(filePath)
+ );
+};
